c0da89316a19eccef42dccb175c5f2d7dd71c48f
nnaveen20242-cpu <nnaveen20242@gmail.com>
2026-03-17 13:28:15 +0530

    Fitst push

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+//This is UC 1
+
+/*
+
+@author Naveen
+@version 1.1
+*/
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Your Code goes here!
+        System.out.println("Hello World");
+
+    }
+}
